534c85f812337908c6958040c86a7673f14988c8
Dhan ilham <132967088+kubokabi@users.noreply.github.com>
2024-07-07 13:17:30 +0700

    Update register.blade.php

--- resources/views/Auth/register.blade.php
+++ resources/views/Auth/register.blade.php
@@ -29,13 +29,6 @@
     <!-- Template Main CSS File -->
     <link href="assets/css/style.css" rel="stylesheet">
 
-    <!-- =======================================================
-  * Template Name: NiceAdmin
-  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-  * Updated: Apr 20 2024 with Bootstrap v5.3.3
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 
 <body>
@@ -51,7 +44,7 @@
                             <div class="d-flex justify-content-center py-4">
                                 <a href="index.html" class="logo d-flex align-items-center w-auto">
                                     <img src="assets/img/logo.png" alt="">
-                                    <span class="d-none d-lg-block">NiceAdmin</span>
+                                    <span class="d-none d-lg-block">Fotocopy</span>
                                 </a>
                             </div><!-- End Logo -->
 
